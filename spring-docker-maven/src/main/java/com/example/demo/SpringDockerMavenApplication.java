229e9fa7b510fac616c56122cd31a40313cef2da
roua99 <35867467+roua99@users.noreply.github.com>
2020-01-24 01:38:24 -0800

    aaaaaaaaaaaa

---
+++ spring-docker-maven/src/main/java/com/example/demo/SpringDockerMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerMavenApplication.class, args);
+	}
+
+}
